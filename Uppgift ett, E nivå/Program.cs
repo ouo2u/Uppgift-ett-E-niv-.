5f5840b7082d4e8593586a042a627a7198653dc6
E-Svensson <edvin.sven123@gmail.com>
2023-02-17 10:16:38 +0100

    Add project files.

---
+++ Uppgift ett, E nivå/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgiftett_Enivå
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in A för rektanglen, alltså bredd");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("skriv in B för rektanglen, alltså höjd");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Arean för rektanglen är{AREAA(A,B)}");
+
+        }
+        static int AREAA (int A, int B)
+        {
+           int AREA = A * B;
+           return AREA;
+        }
+    }
+}
